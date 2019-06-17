4034b0adf8fec5ce0eca253bf8424234c57d9505
Marcus <gandalfgrey@gmx.com>
2019-06-17 11:16:39 -0700

    moved src added read me resource files

---
+++ src/scripts/viewUpdate.js
@@ -0,0 +1,60 @@
+var accordionDescriptions = {
+	'homepage':{
+	  projects:"Hello my name is Marcus Wong, welcome to my website where you can learn more about me, my software and art projects. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+   	  code:"hello",
+   	  about:"in the bio section you will find my resume and briefy summary of who I am",
+	},
+
+	'code':{
+	},
+	
+	'projects':{
+	  'web apps':" hello",
+	  'big data':" big",
+	  'current': "",
+	},	
+	
+	'contact':{
+		email:"email",
+		phone:"phone"
+	},
+
+	'bio':{
+	}
+}
+
+var accordionButtonSection = document.getElementsByClassName('accordion-buttons');
+var panel = document.getElementsByClassName('accordion-panel');
+var panelParagraph = document.getElementById('accordion-description');
+var showing = ""
+
+function loadPage(page){
+ var keys = Object.keys(accordionDescriptions[page])
+ keys.forEach(function(key){
+ 	let nextButton = document.createElement("button");
+ 	nextButton.setAttribute('id', key);
+
+ 	nextButton.addEventListener("click", function() {
+  	if(showing == this.id){
+  	 panel[0].style.visibility = "hidden";  	 
+	 panel[0].style.width = '0%';
+	 panel[0].style.height = '0%';	 
+	 showing = ""
+  	}else{
+  	 panel[0].style.visibility = "visible";  	 
+	 panel[0].style.width = '100%';
+	 panel[0].style.height = '100%';	
+  	 showing = this.id
+  	}	
+    panelParagraph.innerHTML = accordionDescriptions[page][this.id]
+  });
+ 	let span = document.createElement("span");
+ 	span.appendChild(document.createTextNode("/ "));
+	accordionButtonSection[0].appendChild(span);
+
+ 	nextButton.appendChild(document.createTextNode(key));
+ 	accordionButtonSection[0].appendChild(nextButton);	   
+ });
+}
+
+ 
